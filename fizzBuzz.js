0b595e5ed94e7a9d0befa86ed525f8fa14a0402f
marrerap <andrew.marrero@outlook.com>
2021-10-05 10:13:39 -0500

    finised fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,7 +2,18 @@ const prompt = require('prompt-sync')({sigint: true});
 
 const answer = parseInt(prompt('Please enter a number you would like to be FizzBuzzed: '));
 
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    }else if (i % 5 === 0) {
+        console.log('buzz')
+    }else if (i % 3 === 0 ) {
+        console.log('fizz')
+    }else {
+        console.log(i)
+    }
+    
+}
 
 
-console.log(answer)
 
